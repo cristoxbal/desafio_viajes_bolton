8d555c40e2a81feeebef04b0c9e68e8b9115e395
Cristobal Cerda <cristobal@bolton.cl>
2021-12-27 20:23:13 -0300

    Se agrega Typeform, btn-goTop, variable CSS para Âprimary-color

--- js/main.js
+++ js/main.js
@@ -4,11 +4,30 @@ $(function() {
     console.log( "ready!" );
 });
 
-// SMOOTH SCROLL JQUERY
-$('html, body').animate({
-    scrollTop: $('section').offset().top
-  }, 550);
+// Navbar-color
 
+//PROGRESS-BAR
+addEventListener('DOMContentLoaded', () => {
+  const btn_go_top = document.querySelector('#goTop')
+  const progress_bar = document.querySelector('.indicator-scroll')
+  
+  const get_start_pixels = () => document.documentElement.scrollTop || document.body.scrollTop // Contar pixeles desde el Top
+
+  // const goTop = () => {
+  //   if (get_start_pixels() > 0) {
+  //     requestAnimationFrame(goTop)
+  //     // scrollTo(0, get_start_pixels() - (get_start_pixels() / 0.5)) // Animación del scrollTop
+  //     scrollTo(0,0) // Animación del scrollTop
+  //   }
+  // }
+
+   const indicarScroll = () => { // Oculta y muestra btn_goTop
+     if (get_start_pixels() > 300) { btn_go_top.className = 'mostrar'} 
+       else { btn_go_top.className = 'ocultar'}
+   }
+  btn_go_top.addEventListener('click', goTop)
+  window.addEventListener('scroll', indicarScroll)
+})
 
 // TOOLTIPS
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
